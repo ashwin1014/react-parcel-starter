04e2b1ac8e11fc853dad6f5e435c69b0d8c28293
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-10-20 21:13:40 +0530

    added cross-env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const App = ({title}) => (
-    <div className='app'>
-        <h3>{title}</h3>
-    </div>
+const App = ({ title }) => (
+  <div className='app'>
+    <h3>{title}</h3>
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './sass/main.scss';
 
-ReactDOM.render(<App title='React Parcel Starter'/>,  document.getElementById('root'));
+ReactDOM.render(
+  <App title='React Parcel Starter' />,
+  document.getElementById('root')
+);
