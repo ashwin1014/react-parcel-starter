f67400972fffbb82cfef875db8ebc19e644c1329
ashwin1014 <ashwin1014@yahoo.co.in>
2019-12-08 20:53:03 +0530

    added router + route guard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home.Component';
+// import PrivateRoute from './utils/RouteGuard';
 
-const App = ({ title }) => (
-  <div className='app'>
-    <h3>{title}</h3>
-  </div>
+const App = () => (
+  <BrowserRouter>
+    <div className='app-wrapper'>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        {/* <PrivateRoute path="/" authenticated={false} component={HomePage} exact /> */}
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './sass/main.scss';
 
-ReactDOM.render(
-  <App title='React Parcel Starter' />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
