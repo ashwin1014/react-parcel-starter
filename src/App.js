50d55db8dd7a392a9a8abbfe7d7392bd04d53e02
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2020-04-08 23:13:21 +0530

    Updated Packages + added react-helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Suspense } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { Home, NotFound } from './Routes';
 
 const App = ({ title }) => (
-  <div className='app-wrapper'>
+  <HelmetProvider>
+    <div className='app-wrapper'>
+    {/* Use Helmet wherever(any component) you need to change <head> tags */}
+    <Helmet>
+      <title>React Parcel App</title>
+      <meta charSet="utf-8" />
+      <meta name="description" content="React Starter Application built with Parcel Bundler and EsLint + Prettier integrated" />
+    </Helmet>
     <BrowserRouter>
       <Suspense fallback={<div className='center'>Loading...</div>}>
         <Switch>
@@ -14,7 +22,8 @@ const App = ({ title }) => (
         </Switch>
       </Suspense>
     </BrowserRouter>
-  </div>
+    </div>
+  </HelmetProvider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './sass/main.scss';
 
-ReactDOM.render(<App title='React Parcel Starter' />, document.getElementById('root'));
+const MOUNT_NODE = document.getElementById('root');
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App title='React Parcel Starter' />
+    </React.StrictMode>
+ , MOUNT_NODE);
